f0cd10e7d178b6390bf5f96623a432fe91f57f4a
fengyongcan <fengxiaocan@gmail.com>
2018-04-09 19:57:52 +0800

    更新email

--- emaillibs/src/main/java/com/evil/mail/EmailUtil.java
+++ emaillibs/src/main/java/com/evil/mail/EmailUtil.java
@@ -26,47 +26,48 @@ import javax.mail.internet.MimeMultipart;
  * @描述： 邮件工具类
  */
 public class EmailUtil {
-    private static final String myEmailAccount     = "13434159434@163.com";
-    private static final String myEmailPassword    = "evil123";
-    private static final String myEmailSMTPHost    = "smtp.163.com";
-    private static final String receiveMailAccount = "fengxiaocan@foxmail.com";
+    private static final String myEmailAccount = "13434159434@163.com";
+    private static final String myEmailPassword = "Evil123456";
+    private static final String myEmailSMTPHost = "smtp.163.com";
 
     /**
      * 发送邮件
      *
-     * @param appName   app名字
+     * @param title 标题
      * @param mailContent 内容
-     *
-     * @throws Exception
+     * @param receiveMailAccount 收件人邮箱
      */
-    public static void sendMail2Me(String appName, String mailContent)
+    public static void sendMail2Other(String title,String mailContent,String receiveMailAccount)
     {
         try {
             Properties props = new Properties(); //可以加载一个配置文件
             // 使用smtp：简单邮件传输协议
-            props.put("com.evil.mail.smtp.host", myEmailSMTPHost);//存储发送邮件服务器的信息
-            props.put("com.evil.mail.smtp.auth", "true");//同时通过验证
-            props.put("com.evil.mail.smtp.ssl.enable", "true");//（注意）163邮箱只能用ssl！！！
+            props.put("mail.smtp.host",myEmailSMTPHost);//存储发送邮件服务器的信息
+            props.put("mail.smtp.auth","true");//同时通过验证
+            props.put("mail.smtp.ssl.enable","true");//（注意）163邮箱只能用ssl！！！
+
+            props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+            props.setProperty("mail.smtp.host", myEmailSMTPHost);   // 发件人的邮箱的 SMTP 服务器地址
+            props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
 
             Session session = Session.getInstance(props);//根据属性新建一个邮件会话
             session.setDebug(true); //有他会打印一些调试信息。
 
             MimeMessage message = new MimeMessage(session);//由邮件会话新建一个消息对象
             message.setFrom(new InternetAddress(myEmailAccount));//设置发件人的地址
-            message.setRecipient(Message.RecipientType.TO,
-                                 new InternetAddress(receiveMailAccount));//设置收件人,并设置其接收类型为TO
-            message.setSubject(appName + "出现BUG");//设置标题
+            message.setRecipient(Message.RecipientType.TO,new InternetAddress(receiveMailAccount));//设置收件人,并设置其接收类型为TO
+            message.setSubject(title);//设置标题
             //设置信件内容
             //        message.setText(mailContent); //发送 纯文本 邮件
-            message.setContent(mailContent, "text/html;charset=UTF-8"); //发送HTML邮件，内容样式比较丰富
+            message.setContent(mailContent,"text/html;charset=UTF-8"); //发送HTML邮件，内容样式比较丰富
             message.setSentDate(new Date());//设置发信时间
             message.saveChanges();//存储邮件信息
 
             //发送邮件
             //        Transport transport = session.getTransport("smtp");
             Transport transport = session.getTransport();
-            transport.connect(myEmailAccount, myEmailPassword);
-            transport.sendMessage(message, message.getAllRecipients());//发送邮件,其中第二个参数是所有已设好的收件人地址
+            transport.connect(myEmailAccount,myEmailPassword);
+            transport.sendMessage(message,message.getAllRecipients());//发送邮件,其中第二个参数是所有已设好的收件人地址
             transport.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -76,31 +77,39 @@ public class EmailUtil {
     /**
      * 发送邮件
      *
-     * @param fromMail    发送方Email地址，比如“yemengsky@163.com”
-     * @param user        用户名（一般同上），比如“yemengsky@163.com”
-     * @param password    密码（现在一般都用授权码，根据邮件提供商不同而不同）
-     * @param toMail      接收方Email地址，比如“yemengsky@gmail.com”
-     * @param smtpAddr    发送方stmp地址，比如“smtp.163.com”
-     * @param mailTitle   邮件主题
+     * @param fromMail 发送方Email地址，比如“yemengsky@163.com”
+     * @param user 用户名（一般同上），比如“yemengsky@163.com”
+     * @param password 密码（现在一般都用授权码，根据邮件提供商不同而不同）
+     * @param toMail 接收方Email地址，比如“yemengsky@gmail.com”
+     * @param smtpAddr 发送方stmp地址，比如“smtp.163.com”
+     * @param mailTitle 邮件主题
      * @param mailContent 内容
-     *
      * @throws Exception
      */
-    public static void sendMail(String fromMail,
-                                String user,
-                                String password,
-                                String toMail,
-                                String smtpAddr,
-                                String mailTitle,
-                                String mailContent)
-            throws Exception
+    public static void sendMail(
+            String fromMail,
+            String user,
+            String password,
+            String toMail,
+            String smtpAddr,
+            String mailTitle,
+            String mailContent
+    ) throws Exception
     {
 
         Properties props = new Properties(); //可以加载一个配置文件
         // 使用smtp：简单邮件传输协议
-        props.put("com.evil.mail.smtp.host", smtpAddr);//存储发送邮件服务器的信息
-        props.put("com.evil.mail.smtp.auth", "true");//同时通过验证
-        props.put("com.evil.mail.smtp.ssl.enable", "true");//（注意）163邮箱只能用ssl！！！
+        //        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+        //        props.setProperty("mail.smtp.host", myEmailSMTPHost);   // 发件人的邮箱的 SMTP 服务器地址
+        //        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
+
+        props.put("mail.smtp.host",smtpAddr);//存储发送邮件服务器的信息
+        props.put("mail.smtp.auth","true");//同时通过验证
+        props.put("mail.smtp.ssl.enable","true");//（注意）163邮箱只能用ssl！！！
+
+        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", smtpAddr);   // 发件人的邮箱的 SMTP 服务器地址
+        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
 
         Session session = Session.getInstance(props);//根据属性新建一个邮件会话
         session.setDebug(true); //有他会打印一些调试信息。
@@ -108,19 +117,20 @@ public class EmailUtil {
         MimeMessage message = new MimeMessage(session);//由邮件会话新建一个消息对象
         message.setFrom(new InternetAddress(fromMail));//设置发件人的地址
         message.setRecipient(Message.RecipientType.TO,
-                             new InternetAddress(toMail));//设置收件人,并设置其接收类型为TO
+                             new InternetAddress(toMail)
+        );//设置收件人,并设置其接收类型为TO
         message.setSubject(mailTitle);//设置标题
         //设置信件内容
         //        message.setText(mailContent); //发送 纯文本 邮件
-        message.setContent(mailContent, "text/html;charset=UTF-8"); //发送HTML邮件，内容样式比较丰富
+        message.setContent(mailContent,"text/html;charset=UTF-8"); //发送HTML邮件，内容样式比较丰富
         message.setSentDate(new Date());//设置发信时间
         message.saveChanges();//存储邮件信息
 
         //发送邮件
         //        Transport transport = session.getTransport("smtp");
         Transport transport = session.getTransport();
-        transport.connect(user, password);
-        transport.sendMessage(message, message.getAllRecipients());//发送邮件,其中第二个参数是所有已设好的收件人地址
+        transport.connect(user,password);
+        transport.sendMessage(message,message.getAllRecipients());//发送邮件,其中第二个参数是所有已设好的收件人地址
         transport.close();
     }
 
@@ -128,32 +138,36 @@ public class EmailUtil {
     /**
      * 发送邮件
      *
-     * @param fromMail    发送方Email地址，比如“yemengsky@163.com”
-     * @param user        用户名（一般同上），比如“yemengsky@163.com”
-     * @param password    密码（现在一般都用授权码，根据邮件提供商不同而不同）
-     * @param toMail      接收方Email地址，比如“yemengsky@gmail.com”
-     * @param smtpAddr    发送方stmp地址，比如“smtp.163.com”
-     * @param mailTitle   邮件主题
+     * @param fromMail 发送方Email地址，比如“yemengsky@163.com”
+     * @param user 用户名（一般同上），比如“yemengsky@163.com”
+     * @param password 密码（现在一般都用授权码，根据邮件提供商不同而不同）
+     * @param toMail 接收方Email地址，比如“yemengsky@gmail.com”
+     * @param smtpAddr 发送方stmp地址，比如“smtp.163.com”
+     * @param mailTitle 邮件主题
      * @param mailContent 内容
-     * @param file        附件
-     *
+     * @param file 附件
      * @throws Exception
      */
-    public static void sendMail(String fromMail,
-                                String user,
-                                String password,
-                                String toMail,
-                                String smtpAddr,
-                                String mailTitle,
-                                String mailContent,
-                                File file)
-            throws Exception
+    public static void sendMail(
+            String fromMail,
+            String user,
+            String password,
+            String toMail,
+            String smtpAddr,
+            String mailTitle,
+            String mailContent,
+            File file
+    ) throws Exception
     {
         Properties props = new Properties(); //可以加载一个配置文件
         // 使用smtp：简单邮件传输协议
-        props.put("com.evil.mail.smtp.host", smtpAddr);//存储发送邮件服务器的信息
-        props.put("com.evil.mail.smtp.auth", "true");//同时通过验证
-        props.put("com.evil.mail.smtp.ssl.enable", "true");//（注意）163邮箱只能用ssl！！！
+        props.put("mail.smtp.host",smtpAddr);//存储发送邮件服务器的信息
+        props.put("mail.smtp.auth","true");//同时通过验证
+        props.put("mail.smtp.ssl.enable","true");//（注意）163邮箱只能用ssl！！！
+
+        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", smtpAddr);   // 发件人的邮箱的 SMTP 服务器地址
+        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
 
         Session session = Session.getInstance(props);//根据属性新建一个邮件会话
         session.setDebug(true); //有他会打印一些调试信息。
@@ -161,7 +175,8 @@ public class EmailUtil {
         MimeMessage message = new MimeMessage(session);//由邮件会话新建一个消息对象
         message.setFrom(new InternetAddress(fromMail));//设置发件人的地址
         message.setRecipient(Message.RecipientType.TO,
-                             new InternetAddress(toMail));//设置收件人,并设置其接收类型为TO
+                             new InternetAddress(toMail)
+        );//设置收件人,并设置其接收类型为TO
         message.setSubject(mailTitle);//设置标题
 
 
@@ -174,8 +189,8 @@ public class EmailUtil {
         contentPart.setText(mailContent);
         multipart.addBodyPart(contentPart);
         //添加附件
-        BodyPart   messageBodyPart = new MimeBodyPart();
-        DataSource source          = new FileDataSource(file);
+        BodyPart messageBodyPart = new MimeBodyPart();
+        DataSource source = new FileDataSource(file);
         //添加附件的内容
         messageBodyPart.setDataHandler(new DataHandler(source));
         //添加附件的标题
@@ -189,39 +204,21 @@ public class EmailUtil {
         message.saveChanges();
 
 
-        MailcapCommandMap mc = (MailcapCommandMap) CommandMap.getDefaultCommandMap();
+        MailcapCommandMap mc = (MailcapCommandMap)CommandMap.getDefaultCommandMap();
         mc.addMailcap("text/html;; x-Java-content-handler=com.sun.com.evil.mail.handlers.text_html");
         mc.addMailcap("text/xml;; x-java-content-handler=com.sun.com.evil.mail.handlers.text_xml");
-        mc.addMailcap("text/plain;; x-java-content-handler=com.sun.com.evil.mail.handlers.text_plain");
-        mc.addMailcap("multipart/*;; x-java-content-handler=com.sun.com.evil.mail.handlers.multipart_mixed");
-        mc.addMailcap("message/rfc822;; x-java-content-handler=com.sun.com.evil.mail.handlers.message_rfc822");
+        mc.addMailcap(
+                "text/plain;; x-java-content-handler=com.sun.com.evil.mail.handlers.text_plain");
+        mc.addMailcap(
+                "multipart/*;; x-java-content-handler=com.sun.com.evil.mail.handlers.multipart_mixed");
+        mc.addMailcap(
+                "message/rfc822;; x-java-content-handler=com.sun.com.evil.mail.handlers.message_rfc822");
         CommandMap.setDefaultCommandMap(mc);
 
         //发送邮件
         Transport transport = session.getTransport();
-        transport.connect(user, password);
-        transport.sendMessage(message, message.getAllRecipients());//发送邮件,其中第二个参数是所有已设好的收件人地址
+        transport.connect(user,password);
+        transport.sendMessage(message,message.getAllRecipients());//发送邮件,其中第二个参数是所有已设好的收件人地址
         transport.close();
-    }
-
-    protected abstract static class TRunnable<T>
-            implements Runnable
-    {
-        protected T t;
-
-        public TRunnable() {
-        }
-
-        public TRunnable(T t) {
-            this.t = t;
-        }
-
-        public T getT() {
-            return t;
-        }
-
-        public void setT(T t) {
-            this.t = t;
-        }
     }
 }
